e5bc380e7c2b9cf8be465c423ee45b0ecb2b0b1f
Noah <noah@sol1.com.au>
2025-09-04 10:18:30 +1000

    changed the global vlan condition slightly, only matching if the vlan site passed in is matching the existing object (i.e. both are none, not just the existing one is none)

--- module/sources/common/source_base.py
+++ module/sources/common/source_base.py
@@ -886,8 +886,9 @@ class SourceBase:
                 if vlan_group.matches_site_cluster(vlan_site, vlan_cluster):
                     vlan_object_by_group = vlan
                     break
-
-            if grab(vlan, "data.site") is None and grab(vlan, "data.group") is None:
+            
+            # try to find a matching global VLAN if the site and group are none
+            if grab(vlan, "data.site") == vlan_data.get("site") and grab(vlan, "data.group") is None:
                 vlan_object_global = vlan
 
         if isinstance(vlan_object_by_site, NetBoxObject):
